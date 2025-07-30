b83bbb6db3f4c918eb9878179725054f0ed96d55
Santiago Arocha <arochas846@gmail.com>
2025-07-30 19:52:48 -0300

    Initial steps

---
+++ test.java
@@ -0,0 +1,22 @@
+
+ public class test {
+    public static void main(String... args) {
+        //built in data types
+        int edad = 32;
+        //english nomenclature
+        double estatura = 1.80;
+        boolean casado = true;
+        //char as a character, use just simple quotes
+        char categoria = 'A';
+        //until 9 characters number
+        long numeroLargo = 455444555;
+        //Class data types
+        //String class, as a String itself, use double quotes
+        String nombre = "Santiago";
+        String numeroTelefono = "000000000";
+        String CI = "9999999-9";
+
+        System.out.println("El que le mete es: " + nombre + "\n" + "de: " + edad + " años" + "\n" + "de categoría: " + categoria);
+        
+    }
+}
