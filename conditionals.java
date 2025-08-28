8d54f39c323153d029a8eed6ebf1f0db5a96567b
Santiago Arocha <arochas846@gmail.com>
2025-08-28 00:14:34 -0300

    Learning

---
+++ conditionals.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class conditionals {
+    public static void main(String... args) {
+        int edad;
+
+        System.out.println("Ingrese su edad: ");
+
+        //suprimir warnings
+        @SuppressWarnings("resource")
+        Scanner teclado = new Scanner(System.in);
+
+        edad = teclado.nextInt();
+
+        if (edad > 18) {
+            System.out.println("Según su edad: " + edad + " es mayor de edad.");
+        } else {
+            if (edad == 18) {
+                System.out.println("Según su edad: " + edad + " es recientemente mayor de edad.");
+            }
+            else {
+                System.out.println("Según su edad: " + edad + " es menor de edad.");
+            }
+        }
+
+        System.out.println("LLegué al final"); 
+    }
+}
