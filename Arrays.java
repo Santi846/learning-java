5e743220bba7a1763c1ea130bbc88ea4d8e2c3cd
Santiago Arocha <arochas846@gmail.com>
2026-01-13 11:36:20 -0300

    Onedimensional array

---
+++ Arrays.java
@@ -0,0 +1,22 @@
+
+public class Arrays {
+    public static void main(String... args) {
+    //define ONEDIMENSIONAL array, and it's size
+       int numeros [] = new int[8];
+
+    //assign values to each position of the array
+        numeros[0] = 5;
+        numeros[1] = 10;
+        numeros[2] = 15;
+        numeros[3] = 20;
+        numeros[4] = 25;
+        numeros[5] = 30;
+        numeros[6] = 35;
+        numeros[7] = 40;
+      
+    //print each value of the array
+        for(int num: numeros){
+            System.out.println(num);
+        }
+    }
+}
