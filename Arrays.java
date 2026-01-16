4c51aa001c990b118970d4981c632b488e44e073
Santiago Arocha <arochas846@gmail.com>
2026-01-16 10:08:37 -0300

    Assing values dinamically

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Arrays {
     public static void main(String... args) {
@@ -5,6 +6,7 @@ public class Arrays {
        int numeros [] = new int[8];
 
     //assign values to each position of the array
+    /*
         numeros[0] = 5;
         numeros[1] = 10;
         numeros[2] = 15;
@@ -13,10 +15,25 @@ public class Arrays {
         numeros[5] = 30;
         numeros[6] = 35;
         numeros[7] = 40;
-      
-    //print each value of the array
-        for(int num: numeros){
-            System.out.println(num);
-        }
+    */
+    //User enters the values for the array
+    System.out.println("Ingrese los valores del vector: ");
+
+    //suprimir warnings
+    @SuppressWarnings("resource")
+    Scanner teclado = new Scanner(System.in);
+
+    //print a particular position of the array
+    // System.out.println("Value at position 4, index 3:" + numeros[3]);
+
+    for (int i = 0; i < numeros.length; i++) {
+            System.out.print("Ingrese un número: ");
+            numeros[i] = teclado.nextInt();
     }
+
+    for (int i = 0; i < numeros.length; i++) {
+            System.out.println("El valor en la posición " + i + " es: " + numeros[i]);
+    }
+    teclado.close();
+}
 }
